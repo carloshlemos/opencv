d30b61e0bc4670e847d38f0116c71f9b5a21ba2c
carloshlemos <chenriquelemos@gmail.com>
2018-04-25 23:27:57 -0300

    Reconhecimento de Faces com OpenCV e Python

--- treinamento.py
+++ treinamento.py
@@ -2,7 +2,8 @@ import cv2
 import os
 import numpy as np
 
-eigenface = cv2.face.EigenFaceRecognizer_create(num_components=50, threshold=2)
+#eigenface = cv2.face.EigenFaceRecognizer_create(num_components=50, threshold=2)
+eigenface = cv2.face.EigenFaceRecognizer_create()
 fisherface = cv2.face.FisherFaceRecognizer_create()
 lbph = cv2.face.LBPHFaceRecognizer_create()
 
